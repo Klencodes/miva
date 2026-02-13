35b1a0bec99bf56a00ba1e956e1d87dab9677585
Joshua Adiabo <klencodes@gmail.com>
2026-02-13 15:16:55 +0000

    Fixes on baseurl

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
@@ -41,17 +41,6 @@ declare global {
   }
 }
 
-// Detect if running in Electron
-const isElectron = typeof window !== 'undefined' && window.electronAPI;
-
-declare global {
-  interface Window {
-    electronAPI?: {
-      getAppInfo: () => Promise<any>;
-    };
-  }
-}
-
 if (typeof window !== 'undefined' && window.electronAPI) {
   // Running in Electron
   window.electronAPI.getAppInfo().then(info => {
@@ -60,18 +49,10 @@ if (typeof window !== 'undefined' && window.electronAPI) {
   });
 }
 
-// Choose the right router based on environment
-const Router = isElectron ? HashRouter : BrowserRouter;
-
-// For BrowserRouter, only set basename if it's not '.' and not empty
-const basename = !isElectron && process.env.PUBLIC_URL && process.env.PUBLIC_URL !== '.' 
-  ? process.env.PUBLIC_URL 
-  : undefined;
-
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <Router basename={basename}>
+      <BrowserRouter>
         <QueryClientProvider client={queryClient}>
             <ThemeProvider>
               <StoreProvider>
@@ -83,7 +64,7 @@ ReactDOM.createRoot(rootElement).render(
               </StoreProvider>
             </ThemeProvider>
         </QueryClientProvider>
-      </Router>
+      </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
 );
